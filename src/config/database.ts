d45643f136dd81853c9474ae7e85b930ed4448ff
ANOOP <anoopmohan51@gmail.com>
2025-11-07 15:13:02 +0530

    data base changes

--- src/config/database.ts
+++ src/config/database.ts
@@ -13,7 +13,7 @@ dotenv.config();
 
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '336'),
+  port: parseInt(process.env.DB_PORT || '3306'),
   database: process.env.DB_NAME || 'activities_db',
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
